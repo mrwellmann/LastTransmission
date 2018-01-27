5703a157ee4f462254bc24892daad3d41c1c7bb8
dennis-jung <dmjung@live.de>
2018-01-27 11:08:16 +0100

    Further development of solar system

---
+++ UnityHololensProject/Assets/Scritpts/TransmitterScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransmitterScript : MonoBehaviour {
+
+    public GameObject TransmitterAntenna;
+
+    public GameObject Signal;
+
+    public bool IsActive { get; set; }
+
+    public void Turn(float delta)
+    {
+        TransmitterAntenna.transform.Rotate(Vector3.up, delta);
+    }
+
+    public void FireMessage()
+    {
+
+        // spawn MessageObject
+        // Add velocity Antenna.Vector3.forward
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ UnityHololensProject/Assets/SignalScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        // if collided with Planet -> Game Over
+        // if collided with Station -> New Station is active 
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
